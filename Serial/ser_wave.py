3cbae0c47e49101e709a78db917ffa9039a7ac99
Calder10 <salvatorecalderaro94@libero.it>
2020-11-19 20:55:30 +0100

    add update and save_result functions

--- Serial/ser_wave.py
+++ Serial/ser_wave.py
@@ -5,9 +5,8 @@ import numpy as np
 from numpy import pi
 import os
 
-
-MAXP=1000
-MAXSTEPS=1000
+MAXP=10000
+MAXSTEPS=10000
 MINP=20
 
 values=np.zeros(MAXP+2) #valori al tempo t
@@ -49,7 +48,7 @@ def init_param():
 Calcolo dei valori iniziali basati su una sinusoide
 """
 def create_line():
-	global tp,values,old_values
+	global tp,values
 	f=2 * pi
 	k=0.0
 	tmp = tp-1
@@ -60,17 +59,52 @@ def create_line():
 		k=k+1
 
 
+"""
+Aggiorna tutti i valori lungo la linea per un numero di volte specificato
+"""
+def update():
+	global ns,tp,values,old_values,new_values
+	dtime=0.3
+	c=1
+	dx=1
+	tau=(c*dtime/dx)
+	sqtau=tau*tau
+	for i in range(1, ns+1):
+		for j in range(1,tp+1):
+			if (j==1 or j==tp):
+				new_values[j]=0
+			else:
+				new_values[j] = (2.0 * values[j]) - old_values[j] + (sqtau * (values[j-1] - (2.0 * values[j]) + values[j+1]));
+
+		for j in range(1,tp+1):
+			old_values[j]=values[j]
+			values[j]=new_values[j]
+
 def print_values():
-	global values,tp,old_values
+	global values,tp
 	for i in range(1,tp+1):
-		print("%6.4f \t" %(values[i]), end=" ")
+		print("%6.4f " %(values[i]), end=" ")
 		if(i % 10 ==0):
 			print("\n")
 
+def save_result():
+	global values
+	path="res/wawe.txt"
+	np.savetxt(path,values)
+
 def main():
+	print("Inizio versione seriale dell'equazione d'onda...\n")
 	init_param()
+	print("Inizializzazione dei punti sull'onda...\n")
 	create_line()
+	print("Aggiornamento di tutti i valori per ogni istante di tempo...\n")
+	update()
+	print("Stampa dei risultati finali...\n")
 	print_values()
+	save_result()
+	print("Fine !")
+	exit(0)
+
 
 if __name__ == "__main__":
     main()
