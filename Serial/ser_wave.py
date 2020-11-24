c54184ba02f096be0bb7ea3aeb3c2c5ef7129b41
Calder10 <salvatorecalderaro94@libero.it>
2020-11-24 11:56:38 +0100

    add plot_wave function

--- Serial/ser_wave.py
+++ Serial/ser_wave.py
@@ -10,11 +10,9 @@ from time import perf_counter as pc
 MAXP=10000
 MAXSTEPS=10000
 MINP=20
-
 values=np.zeros(MAXP+2) #valori al tempo t
 old_values=np.zeros(MAXP+2) #valori al tempo (t-dt)
 new_values=np.zeros(MAXP+2) # valori al tempo (t+dt)
-
 tp=0
 ns=0
 
@@ -26,21 +24,27 @@ def init_param():
 	global tp,ns
 	while(True):
 		x=input("Inserisci il numero di punti dell'onda [%d-%d]--->" %(MINP,MAXP))
-		tp=int(x)
-		if ((tp < MINP) or (tp > MAXP)):
-			print("Errore ! Inserire valori compresi tra %d e %d " %(MINP,MAXP))
+		if(x.isdigit()==True):
+			tp=int(x)
+			if ((tp < MINP) or (tp > MAXP)):
+				print("Errore ! Inserire valori compresi tra %d e %d " %(MINP,MAXP))
+			else:
+				break
+				os.system("clear")
 		else:
-			break
-			os.system("clear")
+			print("Errore ! Inserire un numero intero")
 
 	while(True):
 		y=input("Inserisci il numero di time steps [1-%d]--->" %(MAXSTEPS))
-		ns=int(y)
-		if ((ns < 1) or (ns > MAXSTEPS)):
-			print("Errore ! Inserire valori compresi tra 1 e %d " %(MAXSTEPS))
+		if(y.isdigit()==True):
+			ns=int(y)
+			if ((ns < 1) or (ns > MAXSTEPS)):
+				print("Errore ! Inserire valori compresi tra 1 e %d " %(MAXSTEPS))
+			else:
+				break
+				os.system("clear")
 		else:
-			break
-			os.system("clear")
+			print("Errore ! Inserire un numero intero")
 
 	print("Numero di punti scelti: %d , numero di steps: %d" %(tp,ns))
 
@@ -65,17 +69,18 @@ Aggiorna tutti i valori lungo la linea per un numero di volte specificato
 """
 def update():
 	global ns,tp,values,old_values,new_values
-	dtime=0.3
+	dt=0.3
 	c=1
 	dx=1
-	tau=(c*dtime/dx)
+	tau=(c*dt/dx)
 	sqtau=tau*tau
 	for i in range(1, ns+1):
 		for j in range(1,tp+1):
 			if (j==1 or j==tp):
 				new_values[j]=0
 			else:
-				new_values[j] = (2.0 * values[j]) - old_values[j] + (sqtau * (values[j-1] - (2.0 * values[j]) + values[j+1]));
+				new_values[j] = (2.0 * values[j]) - old_values[j] + (sqtau * (values[j-1]
+				- (2.0 * values[j]) + values[j+1]))
 
 		for j in range(1,tp+1):
 			old_values[j]=values[j]
@@ -101,9 +106,20 @@ def save_result():
 	result=np.zeros(tp)
 	result=values[1:tp+1]
 	path="res/wawe.txt"
-	plt.plot(result)
-	plt.show()
 	np.savetxt(path,result)
+
+def plot_wave():
+	global values,tp,ns
+	time=np.arange(1,tp+1,1)
+	amplitude=values[1:tp+1]
+	plt.title("Wave")
+	plt.xlabel("Position")
+	plt.ylabel("Amplitude")
+	plt.grid(True, which='both')
+	plt.axhline(y=0, color='k')
+	plt.plot(time,amplitude)
+	plt.show()
+
 
 def main():
 	print("Inizio versione seriale dell'equazione d'onda...\n")
@@ -116,6 +132,7 @@ def main():
 	end_time=pc()-start_time
 	print("Stampa dei risultati finali...\n")
 	print_values()
+	plot_wave()
 	save_result()
 	print("Eseguito in {} s ".format(end_time))
 	exit(0)
@@ -123,3 +140,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
